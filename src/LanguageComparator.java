f1163331a20095ad0305b84ac5c3fb98253eff40
Luis Pedro Lira <luispelirak130@gmail.com>
2024-04-04 12:56:59 -0600

    BinarySearchTree Files

---
+++ src/LanguageComparator.java
@@ -0,0 +1,19 @@
+
+import java.util.Comparator;
+
+public class LanguageComparator<K> implements Comparator<K>{
+
+    @Override
+    public int compare(K o1, K o2) {
+        int key1 = (int)o1;
+        int key2 = (int)o2;
+
+        if (key1 == key2)
+            return 0;
+        else if (key1 > key2)
+            return 1;
+        else 
+            return -1;
+    }
+    
+}
